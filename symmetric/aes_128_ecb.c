f80552cec6f514258f7353c5ccde4513858df2a2
Pwint Myat Kay Khine <kaykhine231@gmail.com>
2016-04-13 17:23:41 +0900

    add blowfish

---
+++ symmetric/aes_128_ecb.c
@@ -0,0 +1,13 @@
+#include "crypto.h"
+#include "../common/common.h"
+
+int crypto_init(EVP_CIPHER_CTX **enc, EVP_CIPHER_CTX **dec, unsigned char *key, unsigned char *iv)
+{
+	if (!(*enc = EVP_CIPHER_CTX_new())) handle_errors();
+	if (!(*dec = EVP_CIPHER_CTX_new())) handle_errors();
+
+	if (!(EVP_EncryptInit_ex(*enc, EVP_aes_128_ecb(), NULL, key, iv))) handle_errors();
+	if (!(EVP_DecryptInit_ex(*dec, EVP_aes_128_ecb(), NULL, key, iv))) handle_errors();
+
+	return 1;
+}
